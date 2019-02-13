d5793940a429423150f3c941cd13a928e7814d0c
kevinandrianliu <39858143+kevinandrianliu@users.noreply.github.com>
2019-02-13 23:11:57 +0700

    Doing Lesson 7.2 Task 2.1 and 2.2

---
+++ app/src/main/java/com/example/user/whowroteit/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.user.whowroteit;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    private EditText bookInput;
+    private TextView author;
+    private TextView title;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        bookInput = (EditText) findViewById(R.id.bookInput);
+        title = (TextView) findViewById(R.id.titleText);
+        author = (TextView) findViewById(R.id.authorText);
+    }
+    public void searchBooks (View view){
+        String queryString = bookInput.getText().toString();
+    }
+}
